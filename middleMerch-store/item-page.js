0ef3566a241f5f83b1e8fa34b36d7839a66867a4
Shaneen <shaneencotterell@gmail.com>
2022-10-12 14:51:38 -0400

    Create item-page.js

---
+++ middleMerch-store/item-page.js
@@ -0,0 +1,44 @@
+// const beats = [
+//     {
+//         title: "Beat 1",
+//        Producer: "Middle",
+//         Price: "from 39.99",
+//    },     
+
+//    {
+//       title: "Beat 2",
+//        Producer: "Middle",
+//        Price: "from 39.99",
+//   },
+
+//    {
+//        title: "Beat 3",
+//        Producer: "Middle",
+//        Price: "from 39.99",
+//    },
+
+//     {
+//        title: "Beat 4",
+//         Producer: "Middle",
+//        Price: "from 39.99",
+//   }];
+
+//   for(let each = 0; each<beats.length;each++){
+//     console.log(beats[each]);
+//     }
+
+// WAVEFORM
+// let wavesurfer = wavesurfer.create({
+//     container: '#waveform',
+//     waveColor: 'violet',
+//     progressColor: 'purple',
+//     });
+    
+//     wavesurfer.load("media/Beat_1.wav")
+
+// PLAY AUDIO
+var x = document.querySelector(".myAudio"); 
+function playAudio() { 
+  x.play(); 
+} 
+
